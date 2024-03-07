5a1efbcb0924b3dd3b511eb9eaa0cdd27a0d1470
Loïc Estève <loic.esteve@ymail.com>
2024-03-07 10:58:15 +0100

    Allow helm-ag with rg to find words even independently on ordering

--- layers/+completion/helm/funcs.el
+++ layers/+completion/helm/funcs.el
@@ -165,7 +165,7 @@ If DEFAULT-INPUTP is non nil then the current region or symbol at point
   ;; --line-number forces line numbers (disabled by default on windows)
   ;; no --vimgrep because it adds column numbers that wgrep can't handle
   ;; see https://github.com/syl20bnr/spacemacs/pull/8065
-  (let* ((root-helm-ag-base-command "rg --smart-case --no-heading --color=never --line-number")
+  (let* ((root-helm-ag-base-command "rg --smart-case --no-heading --color=never --line-number --pcre2")
          (helm-ag-base-command (if spacemacs-helm-rg-max-column-number
                                    (concat root-helm-ag-base-command " --max-columns=" (number-to-string spacemacs-helm-rg-max-column-number))
                                  root-helm-ag-base-command)))
